f77f26f9087721dcc585ba272f97da5e74a63800
Jayson Stemmler <jstemm@uw.edu>
2015-07-09 23:31:33 -0700

    first barely working homepage and style

--- index.php
+++ index.php
@@ -1,10 +1,39 @@
 <html>
     <head>
         <title>ENA Data Browser</title>
-        <link href="style.css" rel="stylesheet" type="text/css">
+        <link href="support-files/style.css" rel="stylesheet" type="text/css">
     </head>
     
     <body>
-        This is the ENA Data Browser
+        <div class="container clearfix">
+            
+            <header><h1>ENA Data Browser</h1></header>
+            
+            <?php require($DOCUMENT_ROOT . "support-files/navbar.html"); ?>
+            <div class="main">
+                <p>
+                    Welcome to the data browser for the Atmospheric Radiation Measurement's 
+                    Eastern North Atlantic permanent site. This site is created and maintained
+                    by Jayson Stemmler of the University of Washington 
+                    (<a href="mailto:jstemm@uw.edu">jstemm@uw.edu</a>). You should send him any
+                    questions or issues you have, not ARM.
+                </p>
+
+                <h2>How to use this site</h2>
+
+                <p>
+                    This site provides quick and easy access to a number of data products from the
+                    permanent ARM site in the ENA. The data used in the visualizations were downloaded
+                    from the ARM Data Archive, which can be accessed through the "Order ENA Data" in
+                    the navigation menu.
+                </p>
+
+                <p>
+                    I will provide more information about the use of this site as I build out the 
+                    functionality of the site. Right now, I don't quite know how it's going to work.
+                </p>
+
+            </div>
+        </div>
     </body>
 </html>
