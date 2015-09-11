c6f7e1fc7817de5e9056f5545a102ecace7a0f93
Jayson Stemmler <jstemm@uw.edu>
2015-09-11 15:03:15 -0700

    separate file for keeping track of the cases

---
+++ support-files/cases.php
@@ -0,0 +1,9 @@
+<?php
+    $cases = array();
+
+    $cases['2014-11-30'] = 
+        "Large UHSAS spike in the afternoon, with corresponding
+        spike in CCN data (although time is slightly offset) coinciding
+        with a shift in the wind pattern.";
+
+?>
