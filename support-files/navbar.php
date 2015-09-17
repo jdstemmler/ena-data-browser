0ea6966a18071a75a932008af45322f8eebeca02
Jayson Stemmler <jstemm@uw.edu>
2015-09-17 06:32:13 -0700

    modified the case submission link to open in new tab

--- support-files/navbar.php
+++ support-files/navbar.php
@@ -9,6 +9,7 @@
             </a>
         </li>
         <li><a href="interesting_cases.php">Interesting Cases</a></li>
-        <li><a href="http://goo.gl/forms/ZQtUZ9cXP1">Submit Case</a></li>
+        <li><a href="http://goo.gl/forms/ZQtUZ9cXP1" target="_blank">Submit 
+Case</a></li>
     </ul>
 </nav>
