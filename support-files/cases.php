fa2905557ac7a7bf69f1119924b52d72fdc05c54
Jayson Stemmler <jstemm@uw.edu>
2015-09-23 11:14:16 -0700

    updated some more cases

--- support-files/cases.php
+++ support-files/cases.php
@@ -5,5 +5,12 @@
         "Large UHSAS spike in the afternoon, with corresponding
         spike in CCN data (although time is slightly offset) coinciding
         with a shift in the wind pattern.";
-
+    
+    $cases['2014-11-29'] = 
+        "Spikes in Total Aerosol coincident with drop in wind speed and
+        shift in wind direction.";
+    
+    $cases['2014-11-24'] = 
+        "Very large Total Aerosol spike coincident with change in wind
+        patterns. Spike is seen clearly in CCN data as well.";
 ?>
