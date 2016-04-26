d596d78eaa37e08acc8ad23d15e69dff6d5f00a7
Jayson Stemmler <jstemm@uw.edu>
2016-04-25 21:54:11 -0700

    cleaning up the templates and structure

--- ena_app.py
+++ ena_app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
     # return "<h1 style='color:blue'>Hello World!!</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=8080)
