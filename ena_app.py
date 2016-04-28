e3cf43bf9ac201cafc75b12214de888ceb9f92c8
Jayson Stemmler <jstemm@uw.edu>
2016-04-28 14:18:54 -0700

    making the interesting cases work

--- ena_app.py
+++ ena_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect
+import pandas as pd
 from dateutil.parser import parse
 import datetime
 
@@ -39,6 +40,22 @@ def date_page(date):
             'prev': prev_date}
 
     return render_template('date_page.html', **args)
+
+@app.route('/interesting_cases')
+def cases():
+    table = pd.read_csv('https://docs.google.com/spreadsheets/d/' +
+                    '1j-yL-SXwPXMxEzhrx7plkggPYJ6WlHD3EM7gVte7Ba8/' +
+                    'pub?gid=1851660263&single=true&output=csv',
+                    names=['tstamp', 'case_date', 'description', 'category'],
+                    parse_dates=[0,],
+                    header=0)
+    table['case_dt'] = table['case_date'].apply(lambda x: datetime.datetime.strptime(x, '%m/%d/%Y'))
+    new_table = table.set_index('case_dt')[['description', 'category']].sort_index()
+
+    d = new_table.groupby('category').apply(lambda x: x.to_dict()).to_dict()
+
+    return render_template('interesting_cases.html', cases=d)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
